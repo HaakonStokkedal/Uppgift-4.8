4d1859b774263d74c7eeb8853d618c64596b9f5a
hast06001 <hast06001@E-PW024NW1>
2023-10-20 10:24:57 +0200

    Add project files.

---
+++ Uppgift 4.8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift_4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skirv in en sträng.");
+            string sträng = Console.ReadLine();
+            Console.WriteLine("Skriv in två tecken. (ac t.ex)");
+            string teckenText = Console.ReadLine();
+            string tecken1 = teckenText[..1];
+            string tecken2 = teckenText[1..];
+
+            if (sträng.Contains(tecken1) && sträng.Contains(tecken2))
+                Console.WriteLine($"Strängen innehåller både {tecken1} och {tecken2}.");
+            else if (sträng.Contains(tecken1))
+                Console.WriteLine($"Strängen innehåller endast {tecken1}.");
+            else if (sträng.Contains(tecken2))
+                Console.WriteLine($"Strängen innehåller endast {tecken2}.");
+            else
+                Console.WriteLine("Inget av dina tecken finns i strängen.");
+
+            Console.ReadKey();
+        }
+    }
+}
